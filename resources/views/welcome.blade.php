27fe10f52182bcc8a55ccca3b89dd73d1571913f
Najeh Bedhief <najeh_bedhief@hotmail.fr>
2022-06-09 10:29:25 +0100

    welcome-blade-changement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GitKraken Test
                 </div>
 
                 <div class="links">
